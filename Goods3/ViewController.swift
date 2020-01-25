9b0e195e6b1c79c785a7155974233469ef1be5ae
Kudo Ayana <nyokinyoki41aya@gmail.com>
2020-01-25 19:41:32 +0900

    Initial Commit

---
+++ Goods3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Goods3
+//
+//  Created by 工藤彩名 on 2020/01/25.
+//  Copyright © 2020 Kudo Ayana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
