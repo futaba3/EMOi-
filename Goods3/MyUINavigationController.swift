a71d926834a6cbdfc86f8ceab4297cba1a1d47a0
Futaba3 <nyokinyoki41aya@gmail.com>
2022-11-18 00:29:25 +0900

    fix barColor for iOS15-16

--- Goods3/MyUINavigationController.swift
+++ Goods3/MyUINavigationController.swift
@@ -12,12 +12,28 @@ class MyUINavigationController: UINavigationController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //　ナビゲーションバーの背景色
-        navigationBar.barTintColor = UIColor.init(red: 239/255, green: 96/255, blue: 49/255, alpha: 100/100)
-        // ナビゲーションバーのアイテムの色　（戻る　＜　とか　読み込みゲージとか）
-        navigationBar.tintColor = .white
-        // ナビゲーションバーのフォントと色
-        navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Kano", size: 20), .foregroundColor: UIColor.white]
+        
+        if #available(iOS 15.0, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            // NavigationBarの背景色の設定
+            appearance.backgroundColor = UIColor.init(red: 239/255, green: 96/255, blue: 49/255, alpha: 100/100)
+            UINavigationBar.appearance().standardAppearance = appearance
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+            // NavigationBarのタイトルの文字色の設定
+            appearance.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Kano", size: 20), .foregroundColor: UIColor.white]
+            
+            self.navigationController?.navigationBar.standardAppearance = appearance
+            self.navigationController?.navigationBar.scrollEdgeAppearance = appearance
+            navigationBar.tintColor = .white
+        } else {
+            navigationBar.barTintColor = UIColor.init(red: 239/255, green: 96/255, blue: 49/255, alpha: 100/100)
+            // ナビゲーションバーのアイテムの色　（戻る　＜　とか　読み込みゲージとか）
+            navigationBar.tintColor = .white
+            // ナビゲーションバーのテキストを変更する
+            navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Kano", size: 20), .foregroundColor: UIColor.white]
+        }
+        
     }
     
 }
